ca24ff7ee14bae156df7d1cdeb9f9247cd6762ca
Jaakko Kulmala <jaakko-kulmala@hotmail.com>
2025-07-30 21:26:37 +0000

    popup images

--- searchbar.js
+++ searchbar.js
@@ -254,3 +254,18 @@ const pages = [
     const searching = document.getElementById('search-bar').value;
     console.log('Searching for:',searching);
   }
+
+   document.querySelectorAll(".box img").forEach(image => {
+    image.onclick = () => {
+        const popup = document.querySelector('.popup-image');
+        const popupImg = popup.querySelector('img');
+
+        popup.style.display = 'block';
+        popupImg.src = image.getAttribute('src');
+    }
+});
+
+// Hide popup when clicking outside the image or on the image
+document.querySelector('.popup-image').onclick = () => {
+    document.querySelector('.popup-image').style.display = "none";
+};
--- searchcity.js
+++ searchcity.js
@@ -245,3 +245,18 @@ const pages = [
           suggestion.classList.toggle('active', index === activeIndex);
       });
     }
+
+     document.querySelectorAll(".box img").forEach(image => {
+    image.onclick = () => {
+        const popup = document.querySelector('.popup-image');
+        const popupImg = popup.querySelector('img');
+
+        popup.style.display = 'block';
+        popupImg.src = image.getAttribute('src');
+    }
+});
+
+// Hide popup when clicking outside the image or on the image
+document.querySelector('.popup-image').onclick = () => {
+    document.querySelector('.popup-image').style.display = "none";
+};
