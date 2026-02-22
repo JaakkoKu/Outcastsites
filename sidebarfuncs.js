f827f56d30fefba061bca8f3d0228f4920eb6220
Jaakko Kulmala <jaakko-kulmala@hotmail.com>
2026-02-22 18:25:20 +0000

    Added hamburger menu and made mobile view more pleasing

--- sidebarfuncs.js
+++ sidebarfuncs.js
@@ -1,11 +1,24 @@
 // Variables
 const toggleButton = document.getElementById('toggle-btn')
 const newsidebar = document.getElementById('newsidebar')
+let hamburgerToggleBtn = document.querySelector('.menu_icon_box')
+
+function isMobileView(){
+    return window.innerWidth < 800
+}
+
+function closeMobileSidebar(){
+    newsidebar.classList.remove('open')
+    hamburgerToggleBtn.classList.remove('open')
+}
 
 // Sidebar is closed from the beginning
 function toggleSidebar(event){
     // Prevent closing if clicking a link
     if (event && event.target.tagName === 'A') {
+        return
+    }
+    if (isMobileView()) {
         return
     }
     newsidebar.classList.toggle('close')
@@ -29,19 +42,38 @@ function toggleSubMenu(button){
 
 // Close sidebar when clicking outside of it, but only on smaller screens
 function setnewsidebarInitialState(){
-    if(window.innerWidth < 800){
-        newsidebar.classList.remove('open');
-        newsidebar.classList.add('close');
+    if(isMobileView()){
+        newsidebar.classList.add('open');
+        hamburgerToggleBtn.classList.add('open');
+        newsidebar.classList.remove('close');
+    } else {
+        newsidebar.classList.remove('open')
+        hamburgerToggleBtn.classList.remove('open')
     }
 }
 
-// Add click listener to close sidebar when clicking outside - only once
+hamburgerToggleBtn.onclick = function(){
+    if (!isMobileView()) {
+        return
+    }
+    hamburgerToggleBtn.classList.toggle('open')
+    newsidebar.classList.toggle('open')
+    newsidebar.classList.remove('close')
+}
+// if (hamburgerToggleBtn.classList.contains ('open')){
+//     newsidebar.classList.add('open');
+// }else{
+//     newsidebar.classList.toggle('close');
+//     hamburgerToggleBtn.classList.toggle('close');
+// }
 document.addEventListener('click', (e) => {
-    if(!newsidebar.contains(e.target) && !toggleButton.contains(e.target)){
-        newsidebar.classList.toggle('open')
+    if (!isMobileView()) {
+        return
+    }
+    if (!hamburgerToggleBtn.contains(e.target) && !newsidebar.contains(e.target)) {
+        closeMobileSidebar()
     }
 })
-
 // Close opened sub-menus if user clicks another sub-menu (but not a link)
 function closeAllSubMenus(){
     Array.from(newsidebar.getElementsByClassName('show')).forEach(ul => {
@@ -53,10 +85,10 @@ function closeAllSubMenus(){
 // Searchbar functions
 function search(button){
     button.nextElementSibling.classList.toggle('show')
-    if(newsidebar.classList.contains('close')){
-        newsidebar.classList.toggle('close')
-        toggleButton.classList.toggle('rotate')
-    }
+    // if(newsidebar.classList.contains('close')){
+    //     newsidebar.classList.toggle('close')
+    //     toggleButton.classList.toggle('rotate')
+    // }
 }
 
 // Prevent sidebar and sub-menu from closing when clicking links
